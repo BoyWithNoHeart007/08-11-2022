d4e6db5c3e6b23e62ac6df2ce3823a79ebf0a885
BoyWithNoHeart007 <118186513+BoyWithNoHeart007@users.noreply.github.com>
2022-11-14 08:12:43 -0500

    Add files via upload

---
+++ second3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main(){
+
+    printf(" - - - - -\n");
+    printf("|         |\n");
+    printf("|         |\n");
+    printf("|  Ravi   |\n");
+    printf("|         |\n");
+    printf("|         |\n");
+    printf(" - - - - -\n");
+    
+}
+
---
+++ second4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main(){
+
+       printf(" - - -\n");
+       printf("|     |\n");
+       printf("R     |\n");
+       printf("A     |\n");
+       printf("V     |\n");
+       printf("I     |\n");
+       printf("|     |\n");
+       printf(" - - -\n");
+
+
+
+}
